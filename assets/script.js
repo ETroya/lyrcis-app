6c8a3a4479e6645b895d754a2a3a2b99feb1f3f9
ssadvand <ssadvand@uci.edu>
2020-12-15 21:35:47 -0800

    got the API to work

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,13 @@
-//Get the User Input
-var searchTerms; //The value entered into the search box.
-var trackID; //The ID created in the getTrack function for use in the returnLyrics function. 
-var resultsSection = document.getElementById("results"); //the html div used to display the results of each ajax call
-var backButton = document.getElementById("back-button-container"); //container for button for the user to navigate between results pages
-
 //Get the API key
 // Syrus's API key: ccfe673d50e42db6f9e361f10c95b6b0
-var apiKey = "ccfe673d50e42db6f9e361f10c95b6b0"
-var url = `https://api.musixmatch.com/ws/1.1/?apikey=${apiKey}`;
+var apiKey = "ccfe673d50e42db6f9e361f10c95b6b0";
+var q = "track.lyrics.get?track_id=15953433"
+var url = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/${q}&apikey=${apiKey}`;
+
+
 $.ajax({
-    url: url,
+    url:url,
     type: "GET"
-}).then(function(response) {
+}).then(function(response){
     console.log(response);
 })
-
-
-//Get the API responsive through console log
-
-
