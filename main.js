70e67bae4c60c25381b07505efce0ce22902c4c7
Ankush <ankushchalla@gmail.com>
2020-12-17 22:40:49 -0800

    Adding comments to main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 var apiKey = "5fdfd8b8b33408cad71de26acf2b6c9f";
 
+// Gets track ID for song + artist inputted by user and uses that 
+// ID to find lyrics.
 $("#search").on("click", function(event) {
 	event.preventDefault();
 	var songName = $("#search-song").val();
