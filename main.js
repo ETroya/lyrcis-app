b21545e246426f08eb51dbfb675b3a79a3886aee
Ankush <ankushchalla@gmail.com>
2020-12-17 20:53:57 -0800

    Adding basic search capability.

---
+++ main.js
@@ -0,0 +1,28 @@
+
+var apiKey = "5fdfd8b8b33408cad71de26acf2b6c9f";
+
+$("#search").on("click", function(event) {
+	event.preventDefault();
+	var songName = $("#search-song").val();
+	var artist = $("#search-artist").val();
+	var method = "track.search?";
+	var q = `q_track=${songName}&q_artist=${artist}`;
+	var songURL = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/${method}&${q}&apikey=${apiKey}`;
+
+	$.ajax({
+		url: songURL, 
+		type: "GET"
+	}).then(function(response) {
+		var trackID = JSON.parse(response).message.body.track_list[0].track.track_id;
+		method = "track.lyrics.get?";
+		q = `track_id=${trackID}`;
+		songURL = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/${method}&${q}&apikey=${apiKey}`;
+
+		$.ajax({
+			url: songURL,
+			type: "GET"
+		}).then(function(response) {
+			console.log(JSON.parse(response).message.body.lyrics.lyrics_body);
+		})
+	})
+})
