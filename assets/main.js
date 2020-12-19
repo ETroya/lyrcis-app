1696dfa159745cc5e717fd4e71ec74942a7edcc3
Ankush <ankushchalla@gmail.com>
2020-12-19 11:28:10 -0800

    Adding search bar functionality.

--- assets/main.js
+++ assets/main.js
@@ -3,14 +3,13 @@ var apiKey = "5fdfd8b8b33408cad71de26acf2b6c9f";
 
 // Gets track ID for song + artist inputted by user and uses that 
 // ID to find lyrics.
-$("#search").on("click", function (event) {
+$("#searchBtn").on("click", function (event) {
 	event.preventDefault();
 	var songName = $("#search-song").val();
 	var artist = $("#search-artist").val();
 	var method = "track.search?";
 	var q = `q_track=${songName}&q_artist=${artist}`;
 	var songURL = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/${method}&${q}&apikey=${apiKey}`;
-
 
 	$.ajax({
 		url: songURL,
