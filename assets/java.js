45ed704e238d28f39ad41b3d3a839fc1a5c55468
Edwin Troya <edtroyajr@gmail.com>
2020-12-22 20:48:30 -0800

    removing buttons

---
+++ assets/java.js
@@ -0,0 +1,8 @@
+function albumCover(){
+    document.getElementById("albumCover").addEventListener("click")
+    var text = document.createTextNode()
+    var paragraph = document.getElementById (p)
+    paragraph.appendchild(text);
+
+    
+}
