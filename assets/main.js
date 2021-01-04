54e49672e398f6e160c4df58e6c2366eb72dbbc7
Ankush <ankushchalla@gmail.com>
2021-01-03 22:18:12 -0800

    Adding google translate.

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,35 @@
 var apiKey = "5fdfd8b8b33408cad71de26acf2b6c9f";
 
 function translate(lyrics) {
-  var de = "ankushchalla@gmail.com"
+  // Default translation is Spanish.
   var inputLang = $("#search-lang option:selected").text();
   if (inputLang === "Italian") {
     var lang = "it";
-  }
-  else if (inputLang === "Spanish") {
+  } else if (inputLang === "Spanish") {
     var lang = "es";
-  }
-  else if (inputLang === "French") {
+  } else if (inputLang === "French") {
     var lang = "fr";
   }
-  else {
-    return;
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    "method": "POST",
+    "headers": {
+      "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+      "x-rapidapi-key": "3d49dbd209msh5e2bcaf5544db64p181262jsn89c70bd689e7",
+      "content-type": "application/x-www-form-urlencoded"
+    },
+    "data": {
+      "source": "en",
+      "q": lyrics,
+      "target": lang,
+      "content-encoding": "gzip",
+    }
   }
-  var url = `https://api.mymemory.translated.net/get?q=${lyrics}&langpair=en|${lang}&de=${de}`
-  $.ajax({
-    url: url, 
-    method: "GET"
-  }).then(function(response) {
-    var translation = response.responseData.translatedText;
-    console.log("Translation:", translation);
-    //putting HTML Text box
-    var textBox = document.querySelector(".example4");
-    textBox.textContent = translation
-  })
+  $.ajax(settings).done(function (response) {
+    console.log(response.data.translations[0]);
+  });
 }
 
 // Using MyMemory API.
@@ -86,6 +90,7 @@ function getLyrics() {
     q = `track_id=${trackID}`;
     songURL = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/${method}&${q}&apikey=${apiKey}`;
 
+    // Translate lyrics.
     $.ajax({
       url: songURL,
       type: "GET"
@@ -95,31 +100,31 @@ function getLyrics() {
       var textBox = document.querySelector(".example3");
       textBox.textContent = fullLyrics
       console.log("Lyrics:", fullLyrics);
-      var lyrics = fullLyrics.substring(0,300);
+      var lyrics = fullLyrics.substring(0, 300);
       translate(lyrics);
     })
 
     // Get related artists using artist ID. 
-    method = "artist.related.get?";
-    q = `artist_id=${artistID}`;
-    songURL = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/${method}&${q}&apikey=${apiKey}`;
-    $.ajax({
-      url: songURL,
-      method: "GET"
-    }).then(function(response) {
-      var artists = JSON.parse(response).message.body.artist_list;
-      var artistNames = [];
-      for (var i = 0; i < artists.length; i++) {
-        artistNames[i] = artists[i].artist.artist_name;
-      }
-      console.log("Related artists:", artistNames);
-      var textBox =document.querySelector(".example2")
-      textBox.textContent= artistNames
-    })
+    // method = "artist.related.get?";
+    // q = `artist_id=${artistID}`;
+    // songURL = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/${method}&${q}&apikey=${apiKey}`;
+    // $.ajax({
+    //   url: songURL,
+    //   method: "GET"
+    // }).then(function (response) {
+    //   var artists = JSON.parse(response).message.body.artist_list;
+    //   var artistNames = [];
+    //   for (var i = 0; i < artists.length; i++) {
+    //     artistNames[i] = artists[i].artist.artist_name;
+    //   }
+    //   console.log("Related artists:", artistNames);
+    //   var textBox = document.querySelector(".example2")
+    //   textBox.textContent = artistNames
+    // })
   })
 }
-// When you click on one of these buttons the information will pop out. Album Cover, Related Artist, Lyrics, and Langauage Translator.
 
+// When you click on one of these buttons the information will pop out. Album Cover, Related Artist, Lyrics, and Langauage Translator.
 function openPage(pageName, elmnt, color) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -130,12 +135,11 @@ function openPage(pageName, elmnt, color) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
-  document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = color;
+  // document.getElementById(pageName).style.display = "block";
+  // elmnt.style.backgroundColor = color;
 }
+
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
   getLyrics();
 });
-// Hides everything when yoou click the tabs
-openPage()
