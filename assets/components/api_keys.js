c64aa1ffc06bb9f4af9532faea45f5bea9c634e3
Ankush <ankushchalla@gmail.com>
2021-01-04 21:22:19 -0800

    Resolving merge issues.

--- assets/components/api_keys.js
+++
@@ -1,8 +0,0 @@
-
-function cycleKeys() {
-    var ankushKey = "5fdfd8b8b33408cad71de26acf2b6c9f";
-    var edwinKey = "9a1da9b45c2f0b030d13d1df98e5b710";
-    var syrusKey = "ccfe673d50e42db6f9e361f10c95b6b0";
-
-    var keys = [ankushKey, edwinKey, syrusKey];
-}
--- assets/main.js
+++ assets/main.js
@@ -9,5 +9,4 @@ function main() {
 }
 
 main();
-
-
+  
